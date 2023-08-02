4b6a56b8b89def564df8d00477c4c98a7a42a6eb
Bilal Mirza <bsohail420@gmail.com>
2023-08-02 14:00:54 +0500

    Remove unnecesary files. App.js and Giphy.jsx have been created to give a basic structure to the app.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Giphy from './components/giphy';
+
+const App = () => {
+    return (
+
+    <div className="app">
+        <Giphy />
+    </div>
+
+    )
+}
+
+export default App
+
---
+++ src/components/giphy.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const Giphy = () => {
+    return (
+        <div className="">
+            Giphy
+        </div>
+    )
+};
+
+export default Giphy
