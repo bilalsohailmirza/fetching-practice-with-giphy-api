1e503774f4669a41a6d976946729c44ff121fb97
Bilal Mirza <bsohail420@gmail.com>
2023-08-09 21:36:19 +0500

    Created the basic structure of the app. Displayed the GIFs fetched from the API and provide some styling to the decument body.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Giphy from './components/giphy';
-
+import './App.css'
 const App = () => {
     return (
 
--- src/components/giphy.jsx
+++ src/components/giphy.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { useEffect, useState } from "react";
-
+import '../App.css'
 const Giphy = () => {
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const fetchData = () => {
+            const api_key="FRBw0oIVsmPSFBa82wdhJesZMl2ZDtO2";
+
+            fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${api_key}`)
+                .then(response => response.json())
+                .then(content => {
+                        console.log(content)
+                        setData(content.data)
+                    }
+                )
+            }
+            return () => fetchData();
+        },[])
+
+        const renderGifs = () => {
+
+            return data.map(item => {
+                return (
+                    <div className="gif">
+                        <img src={item.images.fixed_height.url} alt="" />
+                    </div>
+                )
+            })
+            
+        }
+
     return (
-        <div className="">
-            Giphy
+        <div className="container gifs">
+            {renderGifs()}
         </div>
     )
 };
